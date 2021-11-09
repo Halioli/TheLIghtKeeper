2f7c1fd958defa19ffe04b7e8a424518c9d23dd9
Aniol <aniolbarnadat@gmail.com>
2021-11-09 11:48:42 +0100

    Added enemy sprite & continued work with script

--- Assets/Project/Scripts/Enemies/EnemyCharger.cs
+++ Assets/Project/Scripts/Enemies/EnemyCharger.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class EnemyCharger : MonoBehaviour
 {
+    // Public
     public GameObject player;
+    public AttackSystem attackSystem;
+    public float speed = 2f;
+    public float minDistance = 5f;
 
-    // Start is called before the first frame update
-    void Start()
+    // Private
+    private float distance;
+
+    private void Start()
     {
-        
+        distance = Vector2.Distance(player.transform.position, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (distance > minDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+
+        }
+
+        // Update distance
+        distance = Vector2.Distance(transform.position, player.transform.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<HealthSystem>() != null)
+        {
+            attackSystem.DamageHealthSystemWithAttackValue(collision.GetComponent<HealthSystem>());
+        }
     }
 }
