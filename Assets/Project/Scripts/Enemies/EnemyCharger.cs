5aa53ad489dd7f1f75a656952caaeef89e6ee780
Aniol <aniolbarnadat@gmail.com>
2021-11-12 10:19:23 +0100

    Enemy charger & prefab done

--- Assets/Project/Scripts/Enemies/EnemyCharger.cs
+++ Assets/Project/Scripts/Enemies/EnemyCharger.cs
@@ -18,16 +18,18 @@ enum AttackState
 public class EnemyCharger : MonoBehaviour
 {
     // Public
-    public const float ATTACK_RECOVER_TIME = 3f;
-    public const float CHARGE_SPEED = 8f;
-    public const float CHARGE_TIME = 1f;
+    public const float ATTACK_RECOVER_TIME = 2f;
+    public const float CHARGE_SPEED = 12f;
+    public const float CHARGE_TIME = 0.5f;
+    public const float MAX_SPEED = 7f;
+    public const float ACCELERATION = 0.25f;
 
     public GameObject player;
     public AttackSystem attackSystem;
     public Rigidbody2D rigidbody;
-    public float maxSpeed = 5f;
-    public float acceleration = 0.25f;
-    public float attackForce = 10f;
+    public SpriteRenderer spriteRenderer;
+    public float attackForce = 8f;
+    public float distanceToCharge = 4f;
 
     // Private
     private EnemyState enemyState;
@@ -39,14 +41,18 @@ public class EnemyCharger : MonoBehaviour
 
     private float currentSpeed;
     private float currentAttackRecoverTime;
+    private float currentChargeTime;
     private bool hasRecovered;
     private bool collidedWithPlayer;
-    private float currentChargeTime;
 
     private void Start()
     {
-        currentAttackRecoverTime = ATTACK_RECOVER_TIME;
+        player = GameObject.FindGameObjectWithTag("Player");
         rigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        currentAttackRecoverTime = ATTACK_RECOVER_TIME;
+        currentChargeTime = CHARGE_TIME;
         currentSpeed = 1f;
         hasRecovered = false;
         collidedWithPlayer = false;
@@ -61,43 +67,50 @@ public class EnemyCharger : MonoBehaviour
         {
             if (attackState == AttackState.MOVING_TOWARDS_PLAYER)
             {
-                MoveTowardsPlayer();
+                if (currentSpeed < MAX_SPEED)
+                {
+                    currentSpeed += ACCELERATION;
+                }
+                else
+                {
+                    currentSpeed = MAX_SPEED;
+                }
 
                 // Change to CHARGE
-                if (Vector2.Distance(transform.position, player.transform.position) <= 4f)
+                if (Vector2.Distance(transform.position, player.transform.position) <= distanceToCharge)
                 {
-                    //directionOnChargeStart = (player.transform.position - rigidbody.position).normalized;
+                    playerPosition = player.transform.position;
+                    directionOnChargeStart = (playerPosition - rigidbody.position).normalized;
                     attackState = AttackState.CHARGING; // Change state
                 }
             }
             else if (attackState == AttackState.CHARGING)
             {
+                spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
                 if (collidedWithPlayer)
                 {
                     collidedWithPlayer = false;
                     currentChargeTime = CHARGE_TIME; // Reset value
+                    spriteRenderer.color = new Color(1f, 1f, 1f, 1f); // Reset color
                     attackState = AttackState.RECOVERING; // Change state
                 }
             }
             else if (attackState == AttackState.RECOVERING)
             {
+                spriteRenderer.color = new Color(0.36f, 0.36f, 0.36f, 1f);
+                currentSpeed = 0f;
                 Recovering();
             }
-        }
-
-        if (currentSpeed < maxSpeed)
-        {
-            currentSpeed += acceleration;
-        }
-        else
-        {
-            currentSpeed = maxSpeed;
         }
     }
 
     private void FixedUpdate()
     {
-        if (attackState == AttackState.CHARGING)
+        if (attackState == AttackState.MOVING_TOWARDS_PLAYER)
+        {
+            MoveTowardsPlayer();
+        }
+        else if (attackState == AttackState.CHARGING)
         {
             if (collidedWithPlayer)
             {
@@ -113,19 +126,12 @@ public class EnemyCharger : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.collider);
-        if (collision.collider.GetComponent<HealthSystem>() != null)
+        if (collision.collider.GetComponent<HealthSystem>() != null && attackState == AttackState.CHARGING)
         {
             attackSystem.DamageHealthSystemWithAttackValue(collision.collider.GetComponent<HealthSystem>());
 
             collidedWithPlayer = true;
         }
-    }
-
-    private void PushPlayerAndSelf()
-    {
-        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
-        playerRigidbody.AddForce(Vector2.up * attackForce, ForceMode2D.Impulse);
-        rigidbody.AddForce(Vector2.right * attackForce, ForceMode2D.Impulse);
     }
 
     private void MoveTowardsPlayer()
@@ -144,18 +150,30 @@ public class EnemyCharger : MonoBehaviour
         if (currentChargeTime <= 0)
         {
             currentChargeTime = CHARGE_TIME; // Reset value
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f); // Reset color
             attackState = AttackState.RECOVERING; // Change state
         }
+    }
+
+    private void PushPlayerAndSelf()
+    {
+        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+        playerRigidbody.AddForce(directionOnChargeStart * attackForce, ForceMode2D.Impulse);
+        rigidbody.AddForce(-directionOnChargeStart * attackForce, ForceMode2D.Impulse);
     }
 
     private void Recovering()
     {
         currentAttackRecoverTime -= Time.deltaTime;
+        rigidbody.bodyType = RigidbodyType2D.Static;
+
         if (currentAttackRecoverTime <= 0)
         {
             hasRecovered = true;
 
+            rigidbody.bodyType = RigidbodyType2D.Dynamic;
             currentAttackRecoverTime = ATTACK_RECOVER_TIME; // Reset value
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f); // Reset color
             attackState = AttackState.MOVING_TOWARDS_PLAYER; // Change state
         }
     }
