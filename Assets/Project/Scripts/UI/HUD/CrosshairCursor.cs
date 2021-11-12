e688e5fd993e459ab67f453595d74a8fbb36d0fd
David <david.soriano@enti.cat>
2021-11-12 20:29:25 +0100

    Continued pickaxe

--- Assets/Project/Scripts/UI/HUD/CrosshairCursor.cs
+++ Assets/Project/Scripts/UI/HUD/CrosshairCursor.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class CrosshairCursor : MonoBehaviour
 {
+    public GameObject pickAxe;
     // Start is called before the first frame update
-    void Awake()
+    private void Awake()
     {
         Cursor.visible = false;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Vector2 mouseCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mouseCursorPos;
     }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && collision.gameObject.CompareTag("Ore"))
+        {
+            pickAxe.SetActive(true);
+        }
+        else
+        {
+            pickAxe.SetActive(false);
+        }
+    }*/
 }
