9f21c9469e531caec4a82dfe1fa3b1e8c74ddad6
David Soriano <david.soriano@enti.cat>
2021-11-18 23:17:25 +0100

    Fixed Attack problems

--- Assets/Project/Scripts/Tools/TeleportAnimartion.cs
+++ Assets/Project/Scripts/Tools/TeleportAnimartion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationPlayerMining : MonoBehaviour
+public class TeleportAnimartion : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
