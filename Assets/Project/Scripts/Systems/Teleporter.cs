f481baebd063d0947da5104904cada89db5babc0
David <david.soriano@enti.cat>
2021-11-19 20:35:00 +0100

    Started to fix teleport

--- Assets/Project/Scripts/Systems/Teleporter.cs
+++ Assets/Project/Scripts/Systems/Teleporter.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-    [SerializeField] private Transform destination;
+    public string name;
 
-    public Transform GetDestination() { return destination; }
+    private bool activated = false;
+    private Vector2 spawnPosition;
+    private Animator animatior;
 
+    private void Start()
+    {
+        spawnPosition = transform.position;
+        animatior = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+    
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Do the activate teleport animation and stay teleport
+            if (!activated)
+            {
+                Debug.Log("AAAAAAA");
+                animatior.SetBool("isActivated", true);
+            }
+        }
+    }
 }
