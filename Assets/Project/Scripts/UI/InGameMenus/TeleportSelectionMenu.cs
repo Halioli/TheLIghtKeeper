67444f38171228332e9fa4258b3ae3a7225c6da1
Aniol <aniolbarnadat@gmail.com>
2021-11-30 11:26:05 +0100

    Continuing work with script

--- Assets/Project/Scripts/UI/InGameMenus/TeleportSelectionMenu.cs
+++ Assets/Project/Scripts/UI/InGameMenus/TeleportSelectionMenu.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeleportSelectionMenu : MonoBehaviour
 {
     // Private Attributes
+    private Teleporter teleporter;
     private List<GameObject> teleportButtonsGameObjects;
     private RectTransform teleportListRectTransform;
 
-    // Start is called before the first frame update
+    // Public Attributes
+    public GameObject teleportList;
+    public GameObject buttonPrefab;
+
     void Start()
     {
+        teleporter = GameObject.FindGameObjectWithTag("Teleporter").GetComponent<Teleporter>();
         teleportButtonsGameObjects = new List<GameObject>();
-        //teleportListRectTransform = teleportButtonsGameObjects.GetComponent<RectTransform>();
+        teleportListRectTransform = teleportList.GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void UpdateTeleportSelectionMenu()
+    {
+        teleportButtonsGameObjects.Clear();
+
         
     }
 }
