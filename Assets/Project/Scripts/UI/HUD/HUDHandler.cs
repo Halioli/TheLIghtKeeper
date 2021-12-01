b667af10e6f70ac9350f663d55174313ccedd968
Aniol <aniolbarnadat@gmail.com>
2021-12-01 13:02:41 +0100

    Started work with group layouts & fade in/out

--- Assets/Project/Scripts/UI/HUD/HUDHandler.cs
+++ Assets/Project/Scripts/UI/HUD/HUDHandler.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class HUDHandler : MonoBehaviour
 {
     // Private Attributes
+    private const float FADE_TIME = 2f;
+    
+    private float currentFadeTime;
     private int playerHealthValue;
     private int lampTimeValue;
     private int coreTimeValue;
+    private CanvasGroup healthGroup;
+    private CanvasGroup lampGroup;
+    private CanvasGroup coreGroup;
+    private CanvasGroup quickAccessGroup;
 
     // Public Attributes
     public HUDBar healthBar;
@@ -25,23 +32,30 @@ public class HUDHandler : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        currentFadeTime = 0f;
+
         // Initalize health variables
+        healthGroup = GetComponentsInChildren<CanvasGroup>()[0];
         playerHealthValue = playerhealthSystem.GetMaxHealth();
         healthBar.SetMaxValue(playerHealthValue);
         healthBar.UpdateText(CheckTextForZeros(playerHealthValue.ToString()));
 
         // Initialize lamp variables
+        lampGroup = GetComponentsInChildren<CanvasGroup>()[1];
         lampTimeValue = (int)lamp.GetLampTimeRemaining();
         lampBar.SetMaxValue(lampTimeValue);
         lampBar.UpdateText(CheckTextForZeros(lampTimeValue.ToString()));
 
         // Initialize core variables
+        coreGroup = GetComponentsInChildren<CanvasGroup>()[2];
         coreTimeValue = furnace.GetMaxFuel();
         coreBar.SetMaxValue(coreTimeValue);
         coreBar.UpdateText(CheckTextForZeros(coreTimeValue.ToString()));
+
+        // Initialize quick access variables
+        quickAccessGroup = GetComponentsInChildren<CanvasGroup>()[3];
     }
 
-    // Update is called once per frame
     private void Update()
     {
         playerHealthValue = playerhealthSystem.GetHealth();
@@ -70,5 +84,16 @@ public class HUDHandler : MonoBehaviour
     {
         bar.SetValue(value);
         bar.UpdateText(CheckTextForZeros(text));
+    }
+
+    public void ChangeCanvasGroupAlphaToZero(CanvasGroup canvasGroup)
+    {
+        while (currentFadeTime < FADE_TIME)
+        {
+            currentFadeTime += Time.deltaTime;
+
+            canvasGroup.alpha = 1f - Mathf.Clamp01(currentFadeTime / FADE_TIME);
+        }
+        currentFadeTime = 0f;
     }
 }
