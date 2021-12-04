93d138fabfadf987909861f500fd67d81bc8c6a6
David Soriano <david.soriano@enti.cat>
2021-12-04 20:13:32 +0100

    Started Teleport functioanllity

--- Assets/Project/Scripts/Systems/TeleportSystem.cs
+++ Assets/Project/Scripts/Systems/TeleportSystem.cs
@@ -6,16 +6,7 @@ public class TeleportSystem : MonoBehaviour
 {
 
     private Vector3 teleportToGoPosition;
-    
     public List<GameObject> teleports;
 
-    void Start()
-    {
-        //teleports = new List<GameObject>(GameObject.FindGameObjectsWithTag("Teleporter"));
-    }
 
-    private void TeleportPlayer()
-    {
-
-    }
 }
--- Assets/Project/Scripts/UI/InGameMenus/Teleport/TeleportButton.cs
+++ Assets/Project/Scripts/UI/InGameMenus/Teleport/TeleportButton.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportButton : TeleportSelectionMenu
+public class TeleportButton : TeleportSystem
 {
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     public void TeleportToLocation(int buttonNumb)
     {
-        
+        player.transform.position = teleports[buttonNumb].transform.position;
     }
 }
