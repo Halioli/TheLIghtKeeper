b3bb7bba91b460a6243798ee382180e4dd283724
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2021-12-05 09:49:21 +0100

    merging TeleportSystem, TeleportSelectionMenu

---
+++ Assets/Project/Scripts/UI/InGameMenus/TeleporterSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterSystem : MonoBehaviour
+{
+    // Private Attributes
+    private const int MAX_TELEPORTS_TO_UNLOCK = 2;
+    private int currentUnlockedTeleportNumber;
+
+    // Public Attributes
+    public List<Teleporter> teleporters;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
