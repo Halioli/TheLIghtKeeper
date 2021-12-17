bf898a780db69c569235cf53731707c8a098bf68
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2021-12-17 12:48:14 +0100

    Updating LightRod prefab

--- Assets/Project/Scripts/Items/ItemGameObjects/LightRodGameObject.cs
+++ Assets/Project/Scripts/Items/ItemGameObjects/LightRodGameObject.cs
@@ -13,7 +13,8 @@ public class LightRodGameObject : ItemGameObject
     public override void DoFunctionality()
     {
         canBePickedUp = false;
-        StartCoroutine("Functionality");
+        GetComponent<SpriteRenderer>().renderingLayerMask = 1;
+        StartCoroutine(Functionality());
     }
 
 
@@ -24,19 +25,41 @@ public class LightRodGameObject : ItemGameObject
         audioSource.Play();
     }
 
+
+    IEnumerator LightAppears()
+    {
+        float LIGHT_OUTER_RADIUS = light.GetComponent<Light2D>().pointLightOuterRadius;
+        float LIGHT_RADIUS_DIFFERENCE = LIGHT_OUTER_RADIUS - light.GetComponent<Light2D>().pointLightInnerRadius;
+        light.GetComponent<Light2D>().pointLightOuterRadius = 0;
+        light.GetComponent<Light2D>().pointLightInnerRadius = 0;
+        for (float i = 0f; i < LIGHT_OUTER_RADIUS; i += LIGHT_OUTER_RADIUS / 10f)
+        {
+            Debug.Log("wtffffff");
+            light.GetComponent<Light2D>().pointLightOuterRadius = i;
+            light.GetComponent<Light2D>().pointLightInnerRadius = i > LIGHT_RADIUS_DIFFERENCE ? i - LIGHT_RADIUS_DIFFERENCE : 0f;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+
     IEnumerator Functionality()
     {
         FunctionalitySound();
 
+        Vector2 throwDirection = PlayerInputs.instance.GetMousePositionInWorld() - (Vector2)transform.position;
+        throwDirection.Normalize();
         rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
-        rigidbody2D.AddForce(new Vector2(0, -3), ForceMode2D.Impulse);
+        rigidbody2D.AddForce(throwDirection * 4, ForceMode2D.Impulse);
         yield return new WaitForSeconds(0.4f);
         rigidbody2D.bodyType = RigidbodyType2D.Static;
 
 
         GameObject spawnedLight = Instantiate(light, transform);
+        //StartCoroutine(LightAppears());
 
-        float lightTime = 3f;
+        float lightTime = 5f;
+        Color spriteColor = GetComponent<SpriteRenderer>().color;
+        bool disappearing = false;
 
         while (lightTime > 0f)
         {
@@ -46,6 +69,16 @@ public class LightRodGameObject : ItemGameObject
             if (lightTime <= 1f)
             {
                 spawnedLight.GetComponent<Light2D>().intensity -= Time.deltaTime;
+                spriteColor.r -= Time.deltaTime;
+                spriteColor.g -= Time.deltaTime;
+                spriteColor.b -= Time.deltaTime;
+                GetComponent<SpriteRenderer>().color = spriteColor;
+                if (!disappearing)
+                {
+                    StartDespawning(1);
+                    disappearing = true;
+                }
+                
             }
         }
 
