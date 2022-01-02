75bb02ff97ec40e490661bacc24bbe53cbc508d9
Marc <marc.vicente@enti.cat>
2022-01-02 19:16:03 +0100

    Optimized LightBug and Circular Movement Added

--- Assets/Project/Scripts/Map Elements/LightBug.cs
+++ Assets/Project/Scripts/Map Elements/LightBug.cs
@@ -3,35 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
+
+public enum LightBugMovement { LINEAR, CIRCLE };
+
 public class LightBug : Enemy
 {
-    Interpolator lerp;
+    Interpolator horizontalLerp;
+    Interpolator verticalLerp;
+
 
     private Light2D[] pointLightBug;
     private bool isTurnedOn = false;
+
+    public LightBugMovement lightBugMovement;
+
+    //Linear Movement Parameters
+    public float timeToReachEachPoint;
+    public float initialPositionX;
+    public float finalPositionX;
+    public float initialPositionY;
+    public float finalPositionY;
+
+    //Circular Movement Parameters
+    public float speed;
+    public float width;
+    public float height;
+
+    private float timeCounter;
+
+    private float initialIntensity = 0.3f;
+    private float maxIntensity = 1f;
+    private float time;
+    private bool cycleFinished;
     
     void Start()
     {
-        lerp = new Interpolator(5f, Interpolator.Type.SMOOTH);
+        horizontalLerp = new Interpolator(timeToReachEachPoint, Interpolator.Type.SMOOTH);
+        verticalLerp = new Interpolator(0.5f, Interpolator.Type.SMOOTH);
+
         pointLightBug = GetComponentsInChildren<Light2D>();
         healthSystem = GetComponent<BeingHealthSystem>();
         attackSystem = GetComponent<AttackSystem>();
         rigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         player = GameObject.FindGameObjectWithTag("Player");
+        cycleFinished = true;
+
     }
 
     void Update()
     {
-        LightBugMovement();
-        if (isTurnedOn)
-        {
-            FadeOut();
-        }
-        else
-        {
-            FadeIn();
-        }
+        DoLightBugMovement();
 
         if (healthSystem.IsDead())
         {
@@ -39,56 +61,71 @@ public class LightBug : Enemy
         }
     }
 
-    private void LightBugMovement()
+    private void DoLightBugMovement()
     {
-        lerp.Update(Time.deltaTime);
-        if (lerp.isMinPrecise)
-            lerp.ToMax();
-        else if (lerp.isMaxPrecise)
-            lerp.ToMin();
-
-        transform.position = new Vector3(10f - 20f * lerp.Value, 0f, 0f);
-    }
-
-    IEnumerator FadeIn()
-    {
-        Interpolator lightFadeLerp = new Interpolator(1f, Interpolator.Type.SIN);
-        lightFadeLerp.ToMax();
-        while (!lightFadeLerp.isMaxPrecise)
+        if(lightBugMovement == LightBugMovement.LINEAR)
         {
-            lightFadeLerp.Update(Time.deltaTime);
-            //DO STUFF HERE
-            pointLightBug[0].intensity = lightFadeLerp.Inverse + 0.5f;
-            pointLightBug[1].intensity = lightFadeLerp.Inverse * 0.6f;
-            //WAIT A FRAME
-            yield return null;
+            UpdateInterpolators();
+            transform.position = new Vector3(initialPositionX + (finalPositionX - initialPositionX) * horizontalLerp.Value, initialPositionY + (finalPositionX - initialPositionY) * horizontalLerp.Value, 0f);
+            if (initialPositionX - finalPositionX != 0)
+            {
+                transform.position = new Vector3(transform.position.x, (transform.position.y + 1f) - (transform.position.y + 1f) * verticalLerp.Value, 0f);
+            }
+            else
+            {
+                transform.position = new Vector3((transform.position.x + 1f) - (transform.position.x + 1f) * verticalLerp.Value, transform.position.y, 0f);
+            }
+        }
+        else
+        {
+            timeCounter += Time.deltaTime * speed;
+            transform.position = new Vector3(Mathf.Cos(timeCounter) * width, Mathf.Sin(timeCounter) * height, 0);
+
+            
         }
 
-        isTurnedOn = true;
     }
 
-    IEnumerator FadeOut()
-    {
-        Interpolator lightFadeLerp = new Interpolator(1f, Interpolator.Type.SIN);
-        lightFadeLerp.ToMax();
+    //IEnumerator FlashLightAppears()
+    //{
+    //    time = 0f;
+    //    while (time < 1)
+    //    {
+    //        pointLightBug[0].intensity = Mathf.Lerp(initialIntensity, maxIntensity, time);
+    //        pointLightBug[1].intensity = Mathf.Lerp(initialIntensity, maxIntensity, time);
 
-        while (!lightFadeLerp.isMaxPrecise)
-        {
-            lightFadeLerp.Update(Time.deltaTime);
-            //DO STUFF HERE
-            pointLightBug[0].intensity = lightFadeLerp.Value + 0.5f;
-            pointLightBug[1].intensity = (lightFadeLerp.Value + 0.3f) * 0.6f;
-            //WAIT A FRAME
-            yield return null;
-        }
+    //        time += Time.deltaTime;
+    //        yield return new WaitForSeconds(Time.deltaTime);
+    //    }
+    //    time = 0f;
+    //    while (time < 1)
+    //    {
+    //        pointLightBug[0].intensity = Mathf.Lerp(maxIntensity, initialIntensity, time);
+    //        pointLightBug[1].intensity = Mathf.Lerp(maxIntensity, initialIntensity, time);
 
-        isTurnedOn = false;
-    }
+    //        time += Time.deltaTime;
+    //        yield return new WaitForSeconds(Time.deltaTime);
+    //    }
+    //}
 
     protected override void Die()
     {
-        Debug.Log("murio");
         base.Die();
         Destroy(gameObject);
+    }
+
+    private void UpdateInterpolators()
+    {
+        horizontalLerp.Update(Time.deltaTime);
+        if (horizontalLerp.isMinPrecise)
+            horizontalLerp.ToMax();
+        else if (horizontalLerp.isMaxPrecise)
+            horizontalLerp.ToMin();
+
+        verticalLerp.Update(Time.deltaTime);
+        if (verticalLerp.isMinPrecise)
+            verticalLerp.ToMax();
+        else if (verticalLerp.isMaxPrecise)
+            verticalLerp.ToMin();
     }
 }
