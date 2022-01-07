fb333390fdd5d03f48289ca34277acf9f8ae8530
Aniol <aniolbarnadat@gmail.com>
2022-01-07 13:37:02 +0100

    Pause menu pauses the game

--- Assets/Project/Scripts/UI/InGameMenus/PauseMenu.cs
+++ Assets/Project/Scripts/UI/InGameMenus/PauseMenu.cs
@@ -4,11 +4,55 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenu;
 
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerInputs.instance.PlayerPressedPauseButton())
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            } 
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void ClickedResumeButton()
+    {
+        Resume();
+    }
+
+    public void ClickedOptionsButton()
+    {
+
+    }
+
+    public void ClickedMainMenuButton()
+    {
+
+    }
+
+    public void ClickedExitButton()
+    {
+        PlayerInputs.instance.QuitGame();
     }
 }
