ae32575ad3e6b71c1daea3dcb31c445f47654c22
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2022-01-08 21:39:38 +0100

    Started PlayerAudio

---
+++ Assets/Project/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] AudioSource walkingAudioSource;
+    [SerializeField] AudioSource itemPickUpAudioSource;
+    [SerializeField] AudioSource attackAndMineAudioSource;
+    [SerializeField] AudioSource receiveDamageAudioSource;
+    [SerializeField] AudioSource mineBuildUpAudioSource;
+
+    [SerializeField] AudioClip attackAudioSound;
+    [SerializeField] AudioClip mineAudioSound;
+
+    [SerializeField] AudioClip miningBuildUpSound;
+    [SerializeField] AudioClip successCriticalMiningSound;
+    [SerializeField] AudioClip failCriticalMiningSound;
+
+
+
+    private void OnEnable()
+    {
+        // Walking sound
+        PlayerMovement.playPlayerWalkingSoundEvent += PlayWalkingSound;
+        PlayerMovement.pausePlayerWalkingSoundEvent += StopWalkingSound;
+
+        // ItemPickUp sound
+        PlayerInventory.playerPicksUpItemEvent += PlayItemPickUpSound;
+
+        // Attack sound
+        PlayerCombat.playerAttackEvent += PlayAttackSound;
+
+        // ReceiveDamage sound
+        PlayerCombat.playerReceivesDamageEvent += PlayReceiveDamageSound;
+
+        // Mine sound
+        PlayerMiner.playerAttackEvent -= PlayMineSound;
+
+        // MineBuildUp sound
+        PlayerMiner.playerMinesOreEvent += PlayMineBuildUpSound;
+        PlayerMiner.playerSucceessfulMineEvent += PlaySuccessfulMineSound;
+        PlayerMiner.playerFailMineEvent += PlayFailMineSound;
+    }
+
+
+    private void OnDisable()
+    {
+        // Walking sound
+        PlayerMovement.playPlayerWalkingSoundEvent -= PlayWalkingSound;
+        PlayerMovement.pausePlayerWalkingSoundEvent -= StopWalkingSound;
+
+        // ItemPickUp sound
+        PlayerInventory.playerPicksUpItemEvent -= PlayItemPickUpSound;
+
+        // Attack sound
+        PlayerCombat.playerAttackEvent -= PlayAttackSound;
+
+        // ReceiveDamage sound
+        PlayerCombat.playerReceivesDamageEvent -= PlayReceiveDamageSound;
+
+        // Mine sound
+        PlayerMiner.playerAttackEvent -= PlayMineSound;
+
+        // MineBuildUp sound
+        PlayerMiner.playerMinesOreEvent -= PlayMineBuildUpSound;
+        PlayerMiner.playerSucceessfulMineEvent -= PlaySuccessfulMineSound;
+        PlayerMiner.playerFailMineEvent -= PlayFailMineSound;
+    }
+
+
+
+    // Walking sound
+    public void PlayWalkingSound()
+    {
+        walkingAudioSource.Play();
+    }
+
+    public void StopWalkingSound()
+    {
+        walkingAudioSource.Stop();
+    }
+
+
+    // ItemPickUp sound
+    public void PlayItemPickUpSound()
+    {
+        itemPickUpAudioSource.Play();
+    }
+
+
+    // Attack sound
+    public void PlayAttackSound()
+    {
+        attackAndMineAudioSource.clip = attackAudioSound;
+        attackAndMineAudioSource.Play();
+    }
+
+    // ReceiveDamage sound
+    public void PlayReceiveDamageSound()
+    {
+        receiveDamageAudioSource.Play();
+    }
+
+
+    // Mine sound
+    public void PlayMineSound()
+    {
+        attackAndMineAudioSource.clip = mineAudioSound;
+        attackAndMineAudioSource.Play();
+    }
+
+    // MineBuildUp sound
+    public void PlayMineBuildUpSound()
+    {
+        mineBuildUpAudioSource.clip = miningBuildUpSound;
+        mineBuildUpAudioSource.Play();
+    }
+
+    public void PlaySuccessfulMineSound()
+    {
+        mineBuildUpAudioSource.clip = successCriticalMiningSound;
+        mineBuildUpAudioSource.Play();
+    }
+
+    public void PlayFailMineSound()
+    {
+        mineBuildUpAudioSource.clip = failCriticalMiningSound;
+        mineBuildUpAudioSource.Play();
+    }
+
+
+}
