975cf7258074d61ceb13833ae936d6fc8f81ccf9
Aniol <aniolbarnadat@gmail.com>
2022-01-10 20:11:46 +0100

    Done ore respawner game object

--- Assets/Project/Scripts/Map Elements/OreRespawner.cs
+++ Assets/Project/Scripts/Map Elements/OreRespawner.cs
@@ -5,25 +5,65 @@ using UnityEngine;
 public class OreRespawner : Spawner
 {
     // Private Attributes
-    private const float TIME_BETWEEN_RESPAWN = 5f;
+    private const float OVERLAP_CIRCLE_RADIUS = 1f;
 
-    private Transform respawnTransform;
+    private Vector3 respawnPosition;
+    private Quaternion respawnRotation;
+    private Collider2D returnedCollider;
 
     // Public Attributes
+    public float timeBetweenRespawns = 2f;
     public GameObject oreToRespawn;
+    public LayerMask defaultLayerMask;
 
     private void Start()
     {
         canSpawn = true;
-        respawnTransform = GetComponent<Transform>();
+        respawnPosition = gameObject.transform.position;
+        respawnRotation = gameObject.transform.rotation;
 
-        SetSpawnCooldown(TIME_BETWEEN_RESPAWN);
+        SetSpawnCooldown(timeBetweenRespawns);
         Spawn();
     }
 
     protected override void Spawn()
     {
-        Instantiate(oreToRespawn, respawnTransform);
+        Instantiate(oreToRespawn, respawnPosition, respawnRotation);
         canSpawn = false;
+        StartCoroutine(WaitForRespawn());
+    }
+
+    private void CheckIfCanSpawn()
+    {
+        returnedCollider = ReturnOverlapedColliders();
+
+        if (returnedCollider.CompareTag("Ore"))
+        {
+            canSpawn = false;
+        }
+        else
+        {
+            canSpawn = true;
+        }
+    }
+
+    private Collider2D ReturnOverlapedColliders()
+    {
+        return Physics2D.OverlapCircle(respawnPosition, OVERLAP_CIRCLE_RADIUS, defaultLayerMask);
+    }
+
+    IEnumerator WaitForRespawn()
+    {
+        do
+        {
+            yield return new WaitForSeconds(spawnCooldown);
+
+            CheckIfCanSpawn();
+            if (canSpawn)
+            {
+                Spawn();
+            }
+
+        } while (!canSpawn);
     }
 }
