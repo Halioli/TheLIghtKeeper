d5cff329d34f96e766047ede71a00ad276911ff7
Aniol <aniolbarnadat@gmail.com>
2022-01-12 11:22:53 +0100

    Trying to make upgrades work

--- Assets/Project/Scripts/UI/InGameMenus/UpgradeMenuCanvas.cs
+++ Assets/Project/Scripts/UI/InGameMenus/UpgradeMenuCanvas.cs
@@ -32,7 +32,25 @@ public class UpgradeMenuCanvas : MonoBehaviour
     public void UpgradeBranchIsSelected(int index)
     {
         upgradesSystem.UpgradeBranchIsSelected(index);
-        
-        Init(upgradesSystem.upgradeBranches);
+
+        UpdateUpgradeButton(upgradesSystem.upgradeBranches, index);
+    }
+
+    private void UpdateUpgradeButton(List<UpgradeBranch> upgradeBranches, int index)
+    {
+        upgradeBranches[index].Init(index);
+
+        Upgrade upgrade = upgradeBranches[index].GetCurrentUpgrade();
+        Sprite[] sprites = new Sprite[upgrade.requiredItems.Count];
+        string[] amounts = new string[upgrade.requiredItems.Count];
+        int j = 0;
+
+        foreach (KeyValuePair<Item, int> requiredItemPair in upgrade.requiredItems)
+        {
+            sprites[j] = requiredItemPair.Key.sprite;
+            amounts[j] = requiredItemPair.Value.ToString();
+            ++j;
+        }
+        upgradeButtons[index].UpdateButtonElements(upgrade.upgradeDescription, sprites, amounts);
     }
 }
