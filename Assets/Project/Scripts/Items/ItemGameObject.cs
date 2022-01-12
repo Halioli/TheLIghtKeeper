94d280dda89a90cf2681a0aeb1ab48299e32d4e7
tomeuGa <tomeu.garcia@enti.cat>
2022-01-12 15:21:27 +0100

    Fixed Storage inventory bug, realted to upgradedes

--- Assets/Project/Scripts/Items/ItemGameObject.cs
+++ Assets/Project/Scripts/Items/ItemGameObject.cs
@@ -7,8 +7,8 @@ public class ItemGameObject : MonoBehaviour
 {
     // Private Attributes
     private Interpolator lerp;
-    private float lerpDistance = 0.6f;
-    private float halfLerpDistance = 0.3f;
+    private float lerpDistance = 0.3f;
+    private float halfLerpDistance = 0.15f;
     private float startYLerp;
 
     protected Rigidbody2D rigidbody2D;
