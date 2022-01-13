4a5ef01566d8605d2fd48d52231cd59db6790f0e
David Soriano <david.soriano@enti.cat>
2022-01-13 02:59:43 +0100

    Created functional torch puzzle with luxinite as a reward

--- Assets/Project/Scripts/Map Elements/Torch.cs
+++ Assets/Project/Scripts/Map Elements/Torch.cs
@@ -10,6 +10,7 @@ public class Torch : InteractStation
     public Light2D torchLight;
     public TextMeshProUGUI interactText;
     public CircleCollider2D lightRadius;
+    public GameObject linkedRune;
 
     public float torchIntensity = 1f;
     public float turnedOffIntensity = 0f;
@@ -39,7 +40,7 @@ public class Torch : InteractStation
             DoPuzzle();
         }
         PuzzleChecker();
-
+        linkedRune.SetActive(false);
     }
 
     private void Update()
@@ -68,6 +69,8 @@ public class Torch : InteractStation
         if (PuzzleChecker())
         {
             Debug.Log("Puzzle Completed");
+            puzzleSystem.reward.SetActive(true);
+
         }
 
     }
@@ -79,6 +82,10 @@ public class Torch : InteractStation
         turnedOn = false;
         lightRadius.radius = 0.1f;
         animTorch.SetBool("isBurning", false);
+        if (hasToBurn)
+        {
+            linkedRune.SetActive(false);
+        }
     }
 
     void SetTorchLightOn()
@@ -89,6 +96,10 @@ public class Torch : InteractStation
         turnedOn = true;
         lightRadius.radius = 2.8f;
         animTorch.SetBool("isBurning", true);
+        if (hasToBurn)
+        {
+            linkedRune.SetActive(true);
+        }
     }
 
     IEnumerator LightsOn()
