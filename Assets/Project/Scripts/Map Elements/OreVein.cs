809ff3793300d3db1ee10f66701d51ef7fb1f752
Aniol <aniolbarnadat@gmail.com>
2022-01-14 18:01:36 +0100

    Added events to lightCone & lightCricle + continued with coal vein

--- Assets/Project/Scripts/Map Elements/OreVein.cs
+++ Assets/Project/Scripts/Map Elements/OreVein.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OreVein : InteractStation
 {
+    // Public Attributes
     public Item materialVein;
     public Item autoMiner;
+    public GameObject interactText;
+    public TextMeshProUGUI mssgText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Private Attributes
+    private string[] messagesToShow = { "", "No auto-miner found", "Auto-miner placed" };
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerInsideTriggerArea)
+        {
+            GetInput();
+            PopUpAppears();
+        }
+        else
+        {
+            PopUpDisappears();
+        }
+    }
+
+    // Interactive pop up disappears
+    private void PopUpAppears()
+    {
+        interactText.SetActive(true);
+    }
+
+    // Interactive pop up disappears
+    private void PopUpDisappears()
+    {
+        interactText.SetActive(false);
+        mssgText.text = messagesToShow[0];
     }
 }
