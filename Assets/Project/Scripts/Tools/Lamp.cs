59f8a740b16ccb8c0c0e779a09c1bee28fdde405
Marc <marc.vicente@enti.cat>
2022-01-15 19:36:14 +0100

    Fog System Respawn Fixed

--- Assets/Project/Scripts/Tools/Lamp.cs
+++ Assets/Project/Scripts/Tools/Lamp.cs
@@ -70,8 +70,6 @@ public class Lamp : MonoBehaviour
         coneLight.SetAngle(lightAngle);
     }
 
-
-
     private void OnEnable()
     {
         LanternSourceUpgrade.OnLanternSourceUpgrade += UpgradeLampSource;
@@ -98,7 +96,6 @@ public class Lamp : MonoBehaviour
             if (turnOffLanternEvent != null){
                 turnOffLanternEvent();
             }
-            
         }
         else
         {
@@ -126,7 +123,6 @@ public class Lamp : MonoBehaviour
     {
         lampTime = maxLampTime;
     }
-
     public void RefillLampTime(float time)
     {
         if (lampTime + time > maxLampTime)
@@ -192,6 +188,7 @@ public class Lamp : MonoBehaviour
         coneLight.SetIntensity(LIGHT_INTENSITY_ON);
         coneLight.Expand();
     }
+
     public void ActivateCircleLight()
     {
         active = true;
@@ -199,7 +196,6 @@ public class Lamp : MonoBehaviour
         circleLight.SetIntensity(LIGHT_INTENSITY_ON);
         circleLight.Expand();
     }
-
 
     public void DeactivateLampLight()
     {
@@ -234,7 +230,6 @@ public class Lamp : MonoBehaviour
 
         circleLight.Shrink();
     }
-
 
     public float GetLampTimeRemaining()
     {
@@ -274,8 +269,6 @@ public class Lamp : MonoBehaviour
         ++timeLevel;
     }
 
-
-
     IEnumerator LightFlicking()
     {
         if (lampTime > SECONDS_HIGH_FREQUENCY_FLICK)
@@ -298,7 +291,6 @@ public class Lamp : MonoBehaviour
 
             flickerCount = rg.Next(4, 9);
             
-
             for (int i = 0; i < flickerCount; ++i)
             {
                 flickingIntensity = 1f - ((float)rg.NextDouble() * flickerIntensity);
@@ -318,11 +310,6 @@ public class Lamp : MonoBehaviour
             }
 
         }
-
-
         DeactivateConeLight();
-
     }
-
-
 }
