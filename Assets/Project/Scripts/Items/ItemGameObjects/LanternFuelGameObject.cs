d65bb2d1c8e47804401e6f0920e7ca1100d42355
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2022-01-16 21:26:42 +0100

    LanternFuel now shakes and updated UI

--- Assets/Project/Scripts/Items/ItemGameObjects/LanternFuelGameObject.cs
+++ Assets/Project/Scripts/Items/ItemGameObjects/LanternFuelGameObject.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class LanternFuelGameObject : ItemGameObject
 {
     private Lamp playerLamp;
     private float lampTimeToRefill = 5f;
+
+    private float punchTime = 0.25f;
 
     public delegate void LanternFuelSound();
     public static event LanternFuelSound onLanternFuelRefill;
@@ -20,7 +24,9 @@ public class LanternFuelGameObject : ItemGameObject
 
     public override void DoFunctionality()
     {
+        permanentNotPickedUp = true;
         canBePickedUp = false;
+
         playerLamp = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Lamp>();
 
         if (playerLamp.CanRefill())
@@ -28,7 +34,13 @@ public class LanternFuelGameObject : ItemGameObject
             FunctionalitySound();
             playerLamp.RefillLampTime(lampTimeToRefill);
         }
+        StartCoroutine(Shake());
+    }
 
+    IEnumerator Shake()
+    {
+        transform.DOPunchScale(new Vector3(0.2f, 0.2f), punchTime);
+        yield return new WaitForSeconds(punchTime);
         Destroy(gameObject);
     }
 }
