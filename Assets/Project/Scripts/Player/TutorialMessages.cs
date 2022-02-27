f743fd75567f8a58879965ebe28b503d7116db97
Aniol <aniolbarnadat@gmail.com>
2022-02-27 19:48:23 +0100

    Started implemented chat/tutorial mssgs

---
+++ Assets/Project/Scripts/Player/TutorialMessages.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMessages : MonoBehaviour
+{
+    public List<GameObject> gameObjectsToFind;
+    public List<GameObject> gameObjectsFound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < gameObjectsToFind.Count; i++)
+        {
+            if (collision.CompareTag(gameObjectsToFind[i].tag) && 
+                !gameObjectsFound.Contains(gameObjectsToFind[i]))
+            {
+                Debug.Log(gameObjectsToFind[i]);
+                gameObjectsFound.Add(gameObjectsToFind[i]);
+
+                // Send Action
+            }
+        }
+    }
+}
