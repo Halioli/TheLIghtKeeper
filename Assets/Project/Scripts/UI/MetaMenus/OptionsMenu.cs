a25dc260c8af78f79b86ccc669651b5ce29381c4
Aniol <aniolbarnadat@gmail.com>
2022-04-26 09:59:50 +0200

    Changed menus back to previous concept

--- Assets/Project/Scripts/UI/MetaMenus/OptionsMenu.cs
+++ Assets/Project/Scripts/UI/MetaMenus/OptionsMenu.cs
@@ -22,7 +22,6 @@ public class OptionsMenu : MonoBehaviour
     private Resolution[] resolutions;
     private MainMenu mainMenu;
     private bool inMainMenu;
-    private Color[] textColor = { new Color(0.0f, 0.0f, 0.0f), new Color(0.0f, 0.0f, 0.0f) };
 
     private void Start()
     {
